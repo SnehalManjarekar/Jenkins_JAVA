e71d81c62dd96aaa19eff7c2542d2b0c23a88d09
SnehalManjarekar <your-email@example.com>
2026-05-17 16:29:55 +0530

    Fixed ChromeDriver for Jenkins

--- src/test/java/base/baseClass.java
+++ src/test/java/base/baseClass.java
@@ -2,17 +2,30 @@ package base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class baseClass {
 
-        public static WebDriver driver;
+    public static WebDriver driver;
 
-        public void openBrowser() {
-            driver = new ChromeDriver();
-            driver.manage().window().maximize();
-        }
+    public void openBrowser() {
 
-        public void closeBrowser() {
+        ChromeOptions options = new ChromeOptions();
+
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--remote-allow-origins=*");
+
+        driver = new ChromeDriver(options);
+
+        driver.manage().window().maximize();
+    }
+
+    public void closeBrowser() {
+
+        if (driver != null) {
             driver.quit();
         }
     }
+}
